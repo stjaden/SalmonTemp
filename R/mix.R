bda3bd8deac791fc62ffbf5cb5688490274f2389
stjaden <savannah.tjaden@gmail.com>
2019-05-15 12:02:33 -0700

    finished mixing model and test

--- R/mix.R
+++ R/mix.R
@@ -1,13 +1,21 @@
 #' mix
 #'
 #' This model computes the output streamflow and temperature from two different rivers mixing, each with their own streamflow and temperature. 
-#' @param Q  streamflow in cubic feet per second (cfs)
-#' @param temp water temperature in degrees Celcius
+#' @param Q1  streamflow of the recieving water in cubic feet per second (cfs)
+#' @param Q2  streamflow of the tributary water in cubic feet per second (cfs)
+#' @param T1 water temperature of the recieving water in degrees Celcius
+#' @param T2 water temperature of the tributary water in degrees Celcius
 #' @return the function returns the streamflow (cfs) and temperature (degrees C) of the combined rivers 
 #' @example mix()
+#' 
+#' 
 
-mix = function() {
+
+mix = function(Q1, Q2, T1, T2) {
   
+  mix_temp = (T1*(Q1/(Q1+Q2))) + (T2*(Q2/(Q1+Q2)))
+  mix_flow = Q1 + Q2
   
+  return(list(mix_temp, mix_flow))
   
 }
