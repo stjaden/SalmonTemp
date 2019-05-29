7d88433f1537ab21cc8edc2d91e8e2b589987481
stjaden <savannah.tjaden@gmail.com>
2019-05-29 11:55:25 -0700

    Woooo

--- R/dshack_powergen.R
+++ R/dshack_powergen.R
@@ -8,7 +8,7 @@
 #' @param g Acceleration due to gravity (m/sec2) Default is 9.8
 #' @param Kefficiency Turbine Efficiency (0-1) Default is 0.9 for Francis Turbine
 #' @param head net head of water in reservoir (m)
-#' @param flow mass flow rate (m3/sec) 
+#' @param flow flow rate (m3/sec) 
 #' @author Savannah Tjaden
 #' @return Maximum possible power generation (W)
 
@@ -21,7 +21,7 @@ max_power = function(head, flow, rho=1000, g=9.8, Keff=0.9) {
   if (rho < 0) return(NA)
   
   # calculate power
-  result = rho * head * flow * g * Keff
+  result = rho * flow * head * g * Keff
   
   
   return(result)
